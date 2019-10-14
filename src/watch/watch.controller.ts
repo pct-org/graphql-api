2e6a268d66297d2789c752ed4ee288480ee70e2b
Tycho Bokdam <tycho@palmtreecoding.com>
2019-10-14 22:34:46 +0200

    feat: Watch now transcodeds videos for chromecast
    If the coded is not supported by chromecasts it converts it to one that it does

--- src/watch/watch.controller.ts
+++ src/watch/watch.controller.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs'
 import * as path from 'path'
 import { Controller, Get, Res, Req, Param, Logger } from '@nestjs/common'
+import * as ffmpeg from 'fluent-ffmpeg'
 
 import { ConfigService } from '../shared/config/config.service'
 import { TorrentService } from '../shared/torrent/torrent.service'
@@ -8,7 +9,7 @@ import { TorrentService } from '../shared/torrent/torrent.service'
 @Controller()
 export class WatchController {
 
-  private readonly logger = new Logger(TorrentService.name)
+  private readonly logger = new Logger(WatchController.name)
 
   constructor(
     private readonly configService: ConfigService,
@@ -21,7 +22,6 @@ export class WatchController {
     @Res() res,
     @Req() req
   ) {
-    // TODO:: If url contains device=chromecast then use FFMPEG thing to convert video
     this.logger.debug(`[${params._id}]: Watch`)
     const torrent = this.torrentService.torrents.find(torrent => torrent._id === params._id)
 
@@ -106,15 +106,44 @@ export class WatchController {
       })
     }
 
-    if (torrent) {
-      res.send(
-        torrent.file.createReadStream(streamOptions)
-      )
+    const readStream = torrent
+      ? torrent.file.createReadStream(streamOptions)
+      : fs.createReadStream(mediaFileLocation, streamOptions)
+
+    // Check if the device is chromecast
+    const isChromeCast = req.query && req.query.device && req.query.device === 'chromecast'
+
+    if (isChromeCast) {
+      this.logger.debug('Device is chromecast')
+
+      // Double check if it's needed
+      ffmpeg.ffprobe(mediaFileLocation, (err, metadata) => {
+        if (err) {
+          // Send out normal response
+          res.send(readStream)
+        } else {
+          const videoStream = metadata.streams.find(stream => stream.codec_type === 'video')
+
+          // We need to transform it
+          if (['h264'].includes(videoStream.codec_name)) {
+            // Improve the output stream so Chromecast can play it
+            res.send(
+              ffmpeg(readStream)
+                .format('matroska')
+                .addOption('-movflags', 'faststart')
+                .on('progress', progress => this.logger.debug(`[ffmpeg]: Processed until ${progress.timemark}`))
+                .on('error', err => this.logger.error(`[ffmpeg]: ${err.message || err}`))
+                .pipe(null, { end: true })
+            )
+
+          } else {
+            res.send(readStream)
+          }
+        }
+      })
 
     } else {
-      res.send(
-        fs.createReadStream(mediaFileLocation, streamOptions)
-      )
+      res.send(readStream)
     }
   }
 
